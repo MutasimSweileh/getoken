7c1b30b9cb760bae437538b528598a730903a147
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2018-03-21 17:16:18 +0200

    Update test.php

--- test.php
+++ test.php
@@ -72,28 +72,32 @@ $headers = [
                 'User-Agent' => '[FBAN/FB4A;FBAV/37.0.0.0.109;FBBV/11557663;FBDM/{density=1.5,width=480,height=854};FBLC/en_US;FBCR/Android;FBMF/unknown;FBBD/generic;FBPN/com.facebook.katana;FBDV/google_sdk;FBSV/4.4.2;FBOP/1;FBCA/armeabi-v7a=>armeabi;]'
 
 ];
- $ch = curl_init();
-curl_setopt($ch, CURLOPT_HEADER, 1);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_HTTPGET, 1);
-curl_setopt($ch, CURLOPT_FRESH_CONNECT, TRUE);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_URL,"https://b-graph.facebook.com/?include_headers=false&locale=en_US&client_country_code=US");
+
+$login_email = 'your email';
+$login_pass = 'your password';
+
+//Simple cURL
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://m.facebook.com/login.php');
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'charset_test=â‚¬,Â´,â‚¬,Â´,æ°´,Ð”,Ð„&email=' . urlencode($login_email) . '&pass=' . urlencode($login_pass) . '&login=Login');
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS,$vars);  //Post Fields
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept-Charset: utf-8',
+    'Accept-Language: en-us,en;q=0.7,bn-bd;q=0.3',
+    'Accept: text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5'));
+curl_setopt($ch, CURLOPT_COOKIEFILE, getcwd() . '/mirazmac_cookie.txt'); // The cookie file
+curl_setopt($ch, CURLOPT_COOKIEJAR, getcwd() . '/mirazmac_cookie.txt'); // cookie jar
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windo`enter code here`ws; U; Windows NT 5.1; en-US; rv:1.8.1.3) Gecko/20070309 Firefox/2.0.0.3");
+curl_setopt($ch, CURLOPT_REFERER, "http://m.facebook.com");
+$fbMain = curl_exec($ch) or die(curl_error($ch));
+//Blocking Direct Access to file
+if (eregi("fb.php", $_SERVER['PHP_SELF'])) {
+    die("<p><h2>Access Denied!</h2><h4>You don't have right permission to access this file directly.<br/>Contact MiraZ Mac for more information.</h4></p>");
+}
 
-$server_output = curl_exec ($ch);
-//var_dump(curl_getinfo($ch));
-  if (curl_errno($ch)) {
-        echo 'Error:' . curl_error($ch);
-    }
-curl_close ($ch);
 
-//echo  $server_output ;
-$ar = array();
-$vars ="format=json&device_id=0cd272a7-17dc-4766-958e-5b48799250bf&email=2'.$number.'&password='.$number.'&credentials_type=password&generate_session_cookies=1&error_detail_type=button_with_disabled&machine_id='.random_machine_id().'&locale=en_US&client_country_code=US&fb_api_req_friendly_name=authenticate";
-$json = cUrlGetData("https://b-graph.facebook.com/?include_headers=false&locale=en_US&client_country_code=US",$vars,$headers);
-var_dump($json);
 ?>
