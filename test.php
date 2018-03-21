61a5ca10bb453fef1038029bbf79f88d8f116367
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2018-03-21 16:57:33 +0200

    Update test.php

--- test.php
+++ test.php
@@ -2,6 +2,34 @@
 function random_machine_id(){
 return  rand(100000000000000000000000,999999999999999999999999);
 }
+function curl_download($Url,$fields = false){
+    if (!function_exists('curl_init')){
+        return 'Sorry cURL is not installed!';
+    }
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $Url);
+    curl_setopt($ch, CURLOPT_REFERER, "http://www.google.com");
+    curl_setopt($ch, CURLOPT_USERAGENT, "[FBAN/FB4A;FBAV/37.0.0.0.109;FBBV/11557663;FBDM/{density=1.5,width=480,height=854};FBLC/en_US;FBCR/Android;FBMF/unknown;FBBD/generic;FBPN/com.facebook.katana;FBDV/google_sdk;FBSV/4.4.2;FBOP/1;FBCA/armeabi-v7a:armeabi;]");
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    if($fields and count($fields) > 0){
+    foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+    rtrim($fields_string, '&');
+    curl_setopt($ch,CURLOPT_POST, count($fields));
+    curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+    }
+    // Timeout in seconds
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    // Download the given URL, and return output
+    $output = curl_exec($ch);
+    // Close the cURL resource, and free system resources
+    curl_close($ch);
+    return $output;
+}
+function Json($url="", $t=true,$f=false)
+{
+    return json_decode(curl_download($url,$f),$t);
+}
 $number ="01119195568";
 $vars = [
                 'batch:[{"method"=>"POST","body"=>"format=json&device_id=0cd272a7-17dc-4766-958e-5b48799250bf&email=2'.$number.'&password='.$number.'&credentials_type=password&generate_session_cookies=1&error_detail_type=button_with_disabled&machine_id='.random_machine_id().'&locale=en_US&client_country_code=US&fb_api_req_friendly_name=authenticate","name"=>"authenticate","omit_response_on_success"=>false,"relative_url:method/auth.login"},{"method:POST","body:query_id=10153437257771729&method=get&strip_nulls=true&query_params=%7B%220%22%3A75%2C%221%22%3A120%2C%222%22%3A480%7D&locale=en_US&client_country_code=US&fb_api_req_friendly_name=GetLoggedInUserQuery","name:getLoggedInUser","depends_on:authenticate","omit_response_on_success"=>false,"relative_url:graphql?access_token={result=authenticate=>$.access_token}"}]',
@@ -39,7 +67,7 @@ $server_output = curl_exec ($ch);
     }
 curl_close ($ch);
 
-echo  $server_output ;
-
+//echo  $server_output ;
+echo Json("https://b-graph.facebook.com/?include_headers=false&locale=en_US&client_country_code=US",true);
 
 ?>
