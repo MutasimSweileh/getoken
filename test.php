a86669d953f1ddb0a76e0d9342ba7f1c03ea646a
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2018-03-21 13:42:09 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,7 @@
 <?php
+function random_machine_id(){
+return  rand(100000000000000000000000,999999999999999999999999);
+}
 $number ="01119195568";
 $data = [
                 'batch': '[{"method":"POST","body":"format=json&device_id=0cd272a7-17dc-4766-958e-5b48799250bf&email=2'.$number.'&password='.$number.'&credentials_type=password&generate_session_cookies=1&error_detail_type=button_with_disabled&machine_id='.random_machine_id().'&locale=en_US&client_country_code=US&fb_api_req_friendly_name=authenticate","name":"authenticate","omit_response_on_success":false,"relative_url":"method/auth.login"},{"method":"POST","body":"query_id=10153437257771729&method=get&strip_nulls=true&query_params=%7B%220%22%3A75%2C%221%22%3A120%2C%222%22%3A480%7D&locale=en_US&client_country_code=US&fb_api_req_friendly_name=GetLoggedInUserQuery","name":"getLoggedInUser","depends_on":"authenticate","omit_response_on_success":false,"relative_url":"graphql?access_token={result=authenticate:$.access_token}"}]',
@@ -30,7 +33,5 @@ curl_close ($ch);
 
 print  $server_output ;
 
-function random_machine_id(){
-return  rand(100000000000000000000000,999999999999999999999999);
-}
+
 ?>
