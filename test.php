dbf6fed8f13a0f4e3701108cc29704fa9941b627
mohtasmsawilh <mohtasmsawilh1@gmail.com>
2018-03-21 17:02:43 +0200

    Update test.php

--- test.php
+++ test.php
@@ -30,6 +30,28 @@ function Json($url="", $t=true,$f=false)
 {
     return json_decode(curl_download($url,$f),$t);
 }
+function cUrlGetData($url, $post_fields = null, $headers = null) {
+    $ch = curl_init();
+    $timeout = 5;
+    curl_setopt($ch, CURLOPT_URL, $url);
+    if ($post_fields && !empty($post_fields)) {
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
+    }
+    if ($headers && !empty($headers)) {
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    }
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+    $data = curl_exec($ch);
+    if (curl_errno($ch)) {
+        echo 'Error:' . curl_error($ch);
+    }
+    curl_close($ch);
+    return $data;
+}
 $number ="01119195568";
 $vars = [
                 'batch:[{"method"=>"POST","body"=>"format=json&device_id=0cd272a7-17dc-4766-958e-5b48799250bf&email=2'.$number.'&password='.$number.'&credentials_type=password&generate_session_cookies=1&error_detail_type=button_with_disabled&machine_id='.random_machine_id().'&locale=en_US&client_country_code=US&fb_api_req_friendly_name=authenticate","name"=>"authenticate","omit_response_on_success"=>false,"relative_url:method/auth.login"},{"method:POST","body:query_id=10153437257771729&method=get&strip_nulls=true&query_params=%7B%220%22%3A75%2C%221%22%3A120%2C%222%22%3A480%7D&locale=en_US&client_country_code=US&fb_api_req_friendly_name=GetLoggedInUserQuery","name:getLoggedInUser","depends_on:authenticate","omit_response_on_success"=>false,"relative_url:graphql?access_token={result=authenticate=>$.access_token}"}]',
@@ -68,6 +90,7 @@ $server_output = curl_exec ($ch);
 curl_close ($ch);
 
 //echo  $server_output ;
-$json = Json("https://b-graph.facebook.com/?include_headers=false&locale=en_US&client_country_code=US",true);
+$ar = array();
+$json = cUrlGetData("https://b-graph.facebook.com/?include_headers=false&locale=en_US&client_country_code=US");
 var_dump($json);
 ?>
